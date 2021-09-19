492c7ef1d4c8c72ba016b0dba1d774177d614052
Harry Best <harrybest800@gmail.com>
2021-09-19 18:49:20 +0100

    day 4

--- 2016/4/4.test.ts
+++ 2016/4/4.test.ts
@@ -1,4 +1,8 @@
-import { parseRoom } from "./4";
+import { decryptRoomName, isRoomReal, parsePuzzleInput, parseRoom, partOne, shiftCharacter, partTwo } from "./4";
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+
+const loadPuzzleInput = async () => parsePuzzleInput((await readFile(resolve(__dirname, "./input.txt"))).toString());
 
 describe("day 4", () => {
   describe("part one", () => {
@@ -8,6 +12,36 @@ describe("day 4", () => {
         sectorId: 123,
         checksum: "abxyz"
       });
+    });
+
+    it("test typical real room input", () => {
+      expect(isRoomReal(parseRoom("aaaaa-bbb-z-y-x-123[abxyz]"))).toBeTruthy();
+    });
+
+    it("test tied real room input", () => {
+      expect(isRoomReal(parseRoom("a-b-c-d-e-f-g-h-987[abcde]"))).toBeTruthy();
+    });
+
+    it("test not real room input", () => {
+      expect(isRoomReal(parseRoom("totally-real-room-200[decoy]"))).toBeFalsy();
+    });
+
+    it("part one", async () => {
+      expect(partOne(await loadPuzzleInput())).toEqual(173787);
     })
   });
+
+  describe("part two", () => {
+    it("character shift", () => {
+      expect(shiftCharacter('a', 27)).toEqual('b');
+    });
+
+    it("decrypt room name", () => {
+      expect(decryptRoomName(['qzmt', 'zixmtkozy', 'ivhz'], 343)).toEqual("very encrypted name");
+    });
+
+    it("part two", async () => {
+      expect(partTwo(await loadPuzzleInput())).toEqual(548);
+    });
+  })
 });
--- 2016/4/4.ts
+++ 2016/4/4.ts
@@ -1,3 +1,5 @@
+import { string } from "yargs";
+
 type Room = {
   encryptedName: string[],
   sectorId: number,
@@ -21,6 +23,37 @@ export const isRoomReal = (room: Room): boolean => {
     letterCount.set(char, (letterCount.get(char) ?? 0) + 1);
   }
 
-  const sortedLetters = [...letterCount.entries()].sort().sort((a,b) => b[1] - a[1]);
-  return ([0,1,2,3,4].reduce((prev, next) => prev && sortedLetterCount[next][0] == room.checksum[next], true));
+  const sortedLetterCount = [...letterCount.entries()].sort().sort((a,b) => b[1] - a[1]);
+  
+  let isReal = true;
+  for (let i = 0; i < 5; i++) {
+    isReal = isReal && (sortedLetterCount[i][0] == room.checksum[i]);
+  }
+  return isReal;
 }
+
+export const parsePuzzleInput = (puzzleInput: string): Room[] => (
+  puzzleInput
+    .trim()
+    .split("\n")
+    .map(roomString => parseRoom(roomString.trim()))
+);
+
+export const partOne = (rooms: Room[]): number => (
+  rooms
+    .filter(isRoomReal)
+    .reduce((acc, room) => acc + room.sectorId, 0)
+);
+
+export const shiftCharacter = (char: string, shift: number): string => {
+  const startCharCode = 'a'.charCodeAt(0);
+  return String.fromCharCode(startCharCode + ((char.charCodeAt(0) - startCharCode + shift) % 26));
+}
+
+export const decryptRoomName = (encryptedName: string[], sectorId: number): string => {
+  return encryptedName.map(word => [...word].map(letter => shiftCharacter(letter, sectorId)).join("")).join(" ");
+}
+
+export const partTwo = (rooms: Room[]): number => (
+  rooms.filter(isRoomReal).filter(room => decryptRoomName(room.encryptedName, room.sectorId).includes("northpole object storage"))[0].sectorId
+)
